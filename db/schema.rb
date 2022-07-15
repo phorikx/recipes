dd36ff508a0ae2d85f02b0315f42b5158d2c4cb8
Paul Horikx <82879823+phorikx@users.noreply.github.com>
2022-07-15 19:04:47 +0200

    Add Static Pages and update layout
    * Added Static Pages controller
    
    * Modified static pages
    
    * remove recipes controller, revert to erb for application layout
    
    * add static pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_708_163_231) do
-  create_table 'recipes', force: :cascade do |t|
-    t.string 'body'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+ActiveRecord::Schema[7.0].define(version: 2022_07_08_163231) do
+  create_table "recipes", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
