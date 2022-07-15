6782539706a50bb6a020a05d5114ddc7de4ce0eb
Paul Horikx <p.horikx@gmail.com>
2022-07-15 17:06:53 +0200

    Added Static Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
   resources :recipes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'recipes#index'
+  root 'application#hello'
 end
