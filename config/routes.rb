6c8307999cc9afb56e6691a014cf558dc5a00432
Paul Horikx <p.horikx@gmail.com>
2022-08-14 16:39:22 +0200

    added login system

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'users/new'
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
@@ -13,4 +11,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'static_pages#home'
   resources :users
+
+  get '/login',      to: 'sessions#new'
+  post '/login',     to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
